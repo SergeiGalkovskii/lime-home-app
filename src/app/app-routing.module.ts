c6ab05fb025962dd54b4a83eea34f7dee2648d9c
Sergei Galkovskii <sergei.galkovskii@gmail.com>
2020-09-29 02:12:52 +0300

    master: init app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'search'
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
