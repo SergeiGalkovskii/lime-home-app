48a4195e08678c466d27974e6f8326ab51ea53ac
Sergei Galkovskii <sergei.galkovskii@gmail.com>
2020-09-29 02:13:51 +0300

    master: create distance pipe

---
+++ src/app/core/core/pipes/distance.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DistancePipe } from './distance.pipe';
+
+describe('DistanceNumberPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DistancePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
