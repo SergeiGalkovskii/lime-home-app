8a75932e5a7666952e5a8fc054bf911b474180f3
Sergei Galkovskii <sergei.galkovskii@gmail.com>
2020-09-29 02:13:05 +0300

    master: add variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  hereApi: 'https://places.ls.hereapi.com/places/v1',
+  hereApiKey: '78sKKeWSOVZFZEb9jMHHiGtZQbYBWlgRVBugFldpsXU',
+  mapBoxAccessToken: 'pk.eyJ1Ijoic2VyaGlpaGFsa292c2t5aSIsImEiOiJja2ZtdWJ3M3oyMmF5MnJudnhkYjkwOHcwIn0.RjayyJjsri2KRsD3YItwHA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hereApi: 'https://places.ls.hereapi.com/places/v1',
+  hereApiKey: '78sKKeWSOVZFZEb9jMHHiGtZQbYBWlgRVBugFldpsXU',
+  mapBoxAccessToken: 'pk.eyJ1Ijoic2VyaGlpaGFsa292c2t5aSIsImEiOiJja2ZtdWJ3M3oyMmF5MnJudnhkYjkwOHcwIn0.RjayyJjsri2KRsD3YItwHA'
 };
 
 /*
